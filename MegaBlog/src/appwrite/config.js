7b87d066af9319d8720191df5fe84f27e62eac04
thegujju <86185835+thegujju@users.noreply.github.com>
2024-08-30 15:52:28 +0000

    push

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -1,5 +1,5 @@
 import { Client,ID,Databases,Storage,Query } from "appwrite";
-import {conf} from "../conf.js"
+import conf from "../conf/conf"
 
 export class Service{
     client = new Client()
--- MegaBlog/src/components/Header/LogoutBtn.jsx
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService, { AuthService } from '../../appwrite/auth'
-import { logout } from '../../store/authSlice'
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
-    const logoutHandler  = () =>{
-        authService.logout().then(()=>{
+    const logoutHandler = () => {
+        authService.logout().then(() => {
             dispatch(logout())
         })
     }
   return (
-    <div className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler}>
-      <button>Logout</button>
-    </div>
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
   )
 }
 
